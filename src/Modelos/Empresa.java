dcb0be63425f2676bbd8226d6f43249e4eebd59f
supermavp <supermavp@supermavp>
2016-10-14 12:14:52 -0500

    Modificando clases

--- src/Modelos/Empresa.java
+++ src/Modelos/Empresa.java
@@ -9,65 +9,10 @@ package Modelos;
  *
  * @author MECATRONICA
  */
-public class Empresa {
-
-    private String nombre;
-    private String razonSocial;
-    private int nit;
-    private String direccion;
-    private String email;
-    private String telefono;
+public class Empresa extends Persona {
 
     public Empresa() {
-
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getRazonSocial() {
-        return razonSocial;
-    }
-
-    public void setRazonSocial(String razonSocial) {
-        this.razonSocial = razonSocial;
-    }
-
-    public int getNit() {
-        return nit;
-    }
-
-    public void setNit(int nit) {
-        this.nit = nit;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+        super();
     }
 
 }
